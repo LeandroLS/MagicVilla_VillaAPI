17fd147e3e949589030632b804b7cc1f79065f73
Leandro Lima <leandro.lsilva@pagar.me>
2024-08-29 16:50:56 -0300

    adicionando configuração de banco de dados

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Data.Logging;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(opts => {
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 
 // Add services to the container.
 // Log.Logger = new LoggerConfiguration()
