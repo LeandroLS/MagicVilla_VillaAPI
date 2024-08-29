94b337d757a7855e883dabb400822dd77ace474e
Leandro Lima <leandro.lsilva@pagar.me>
2024-08-29 16:15:16 -0300

    Adicionando mais campos na classe Villa e adicionando entity framework

--- Models/DTO/VillaDto.cs
+++ Models/DTO/VillaDto.cs
@@ -9,5 +9,9 @@ public class VillaDto
     [Required]
     public string Name { get; set; }
     public int Occupancy { get; set; }
-    public int Sqft  {get; set; }
+    public int Sqft { get; set; }
+    public string Details { get; set; }
+    public double Rate { get; set; }
+    public string ImageUrl { get; set; }
+    public string Amenity { get; set; }
 }
